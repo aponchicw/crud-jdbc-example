41bff51927e5fe1f6f657aa1d488b32673def117
aponchicw <aponchicw@gmail.com>
2021-05-25 10:41:04 +0600

    Initial commit

---
+++ src/main/java/com/example/crudjdbcexample/CrudJdbcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudjdbcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJdbcExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudJdbcExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudjdbcexample/CrudJdbcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudjdbcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJdbcExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
